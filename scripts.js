458c4e103c75f0d82cd4bfeee84c3d1f9be0683d
filipe <filipelouzada23@gmail.com>
2024-10-04 10:38:57 -0300

    ListaDeTarefas

---
+++ scripts.js
@@ -0,0 +1,52 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const ListaCompleta = document.querySelector('.lista-task')
+
+let ListaItens = []
+
+
+
+function AdicionarTarefa(){
+    ListaItens.push({
+        tarefa: input.value,
+        concluida: false
+    })
+
+    input.value = ''
+
+    MostrarTarefa() 
+}
+
+function MostrarTarefa(){
+    let Li =''
+
+    ListaItens.forEach((item, posicao) => {
+        Li =  Li + `
+
+        <li class="task ${item.concluida && "concluido"}">
+                <img src="./assets/check.jpg" alt="check-na-tarefa" onclick="Checktarefa(${posicao})">
+                <p> ${item.tarefa} </p>
+                <img src="./assets/trash.png" alt="tarefa-para-lixo" onclick="deletarItem(${posicao})">
+                </li> 
+        `  
+    })
+
+    ListaCompleta.innerHTML = Li
+
+}
+
+function Checktarefa(posicao){
+    ListaItens[posicao]. concluida = !ListaItens[posicao]. concluida 
+
+    MostrarTarefa()
+}
+
+function deletarItem(posicao){
+    ListaItens.splice(posicao, 1)
+    
+    MostrarTarefa()
+} 
+
+button.addEventListener('click', AdicionarTarefa)
+
+alert("MUITO FEIOKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
